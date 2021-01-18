602dae1c0881d50611a9002e89108fe27855e1e1
Sandra <sandrakakkarayil01@gmail.com>
2021-01-19 01:25:38 +0530

    create a new user by sending a POST request to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+
+    response_text = "Created new USER, Name:- #{name}, Email:- #{email}"
+    render plain: response_text
+  end
 end
